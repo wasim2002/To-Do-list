53e4c8eb6fd8c7ce89c8643e5af6cf65d40f2300
wasim2002 <wansari4540@gmail.com>
2024-02-19 20:50:54 +0530

    all code done for adding , removing and done

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const input = document.querySelector("#inputF")
-const ul =document.querySelector(".ul")
-let keys = ""
+const ul = document.querySelector("ul")
+
 input.addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
-        keys = input.value
-        addToDo(keys)
+        addToDo(input.value)
     }
 })
+
 function addToDo(e) {
     let li = document.createElement("li")
     li.innerHTML = `
     <div class="textA">${e}</div>
     <i>×</i>
     `
-    ul.appendChild(li)
+    if (input.value != "") {
+        ul.appendChild(li)
+    }
+    input.value = ""
+    li.addEventListener("click", function (e) {
+        let textA = li.querySelector(".textA")
+        textA.classList.toggle("done")
+    })
+    let i = li.querySelector("i")
+    i.addEventListener("click", function () {
+        li.style.display = "none"
+    })
 }
